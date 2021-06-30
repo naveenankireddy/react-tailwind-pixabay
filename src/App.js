14a1a2541ae6609dec2e701471d09c69012383f7
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2021-06-30 06:54:50 +0530

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { CardImage } from "./components/CardImage";
+import { SearchImage } from "./components/SearchImage";
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -14,14 +15,22 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <CardImage key={image.id} image={image} />
-        ))}
-      </div>
+      <SearchImage searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">No results found</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading ....</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <CardImage key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
